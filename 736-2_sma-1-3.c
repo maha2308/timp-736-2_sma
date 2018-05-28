4d751e6ae5cdd347b351e88b0b5cf5bb7ffc8568
Сысолина <736-2_sma@timp2018.novalocal>
2018-05-28 07:48:09 +0000

    commit1-3

---
+++ 736-2_sma-1-3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+	int main()
+{
+	int a,b,resultat;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	resultat=pow(a,b);
+	printf("resultat = %d",resultat);
+	return 0;
+}	
